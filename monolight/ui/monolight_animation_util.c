8c45bc167ae4e9f67e821a60d17f1d6bea1ea89f
Joël Krähemann <jkraehemann@gmail.com>
2019-05-16 03:53:28 +0200

    implemented monolight_animation_util_render_square()

--- monolight/ui/monolight_animation_util.c
+++ monolight/ui/monolight_animation_util.c
@@ -60,6 +60,184 @@ monolight_animation_util_render_square(cairo_t *cr,
 				       gdouble angle, gdouble scale,
 				       guint r, guint g, guint b, guint a)
 {
+  gdouble line_length[5];
+
+  gdouble correction;
+  gdouble frequency;
+  gdouble magnitude;
+  gdouble a_x0, a_y0;
+  gdouble a_width, a_height;
+  gdouble b_x0, b_y0;
+  gdouble b_width, b_height;
+  gdouble c_x0, c_y0;
+  gdouble c_width, c_height;
+  gdouble d_x0, d_y0;
+  gdouble d_width, d_height;
+  gdouble e_x0, e_y0;
+  gdouble e_width, e_height;
+  gdouble max_line_length;
+  guint nth;
+  guint i, k;
+
+  width *= scale;
+  height *= scale;
+  
+  max_line_length = scale * 12.0;
+
+  for(i = 0; i < 5; i++){
+    line_length[i] = max_line_length;
+  }
+  
+  correction = (gdouble) samplerate / (gdouble) buffer_size;
+  magnitude = 0.0;
+  
+  for(nth = buffer_start + 1, i = 0, k = 0; i < buffer_end - buffer_start && nth < buffer_size / 2; i++, nth++){
+    frequency = nth * correction;
+    
+    magnitude += magnitude_buffer[nth];
+    k++;
+
+    if(nth % ((buffer_end - buffer_start) / 5) == 0){
+      line_length[(guint) floor((double) i / (double) (buffer_end - buffer_start) * 5.0)] *= (magnitude / (buffer_end - buffer_start));
+
+      magnitude = 0.0;
+      k = 0;
+    }
+  }
+
+  cairo_set_source_rgba(cr,
+			r, g, b, a);
+
+
+  for(i = 0; i < 4; i++){
+    cairo_push_group(cr);
+    
+    switch(i){
+    case 0:
+    {
+      a_x0 = ((gdouble) width * 0.25) - line_length[0];
+      a_y0 = ((gdouble) height * 0.25) - line_length[0];
+
+      b_x0 = ((gdouble) width * 0.25) - line_length[1];
+      b_y0 = ((gdouble) height * 0.25) + line_length[1];
+
+      c_x0 = ((gdouble) width * 0.25) + line_length[2];
+      c_y0 = ((gdouble) height * 0.25) + line_length[2];
+
+      d_x0 = ((gdouble) width * 0.25) + line_length[3];
+      d_y0 = ((gdouble) height * 0.25) - line_length[3];
+
+      e_x0 = ((gdouble) width * 0.25) - (line_length[4] / 2.0);
+      e_y0 = ((gdouble) height * 0.25) - (line_length[4] / 2.0);
+    }
+    break;
+    case 1:
+    {
+      a_x0 = ((gdouble) width * 0.25) - line_length[0];
+      a_y0 = ((gdouble) height * 0.75) - line_length[0];
+
+      b_x0 = ((gdouble) width * 0.25) - line_length[1];
+      b_y0 = ((gdouble) height * 0.75) + line_length[1];
+
+      c_x0 = ((gdouble) width * 0.25) + line_length[2];
+      c_y0 = ((gdouble) height * 0.75) + line_length[2];
+
+      d_x0 = ((gdouble) width * 0.25) + line_length[3];
+      d_y0 = ((gdouble) height * 0.75) - line_length[3];
+
+      e_x0 = ((gdouble) width * 0.25) - (line_length[4] / 2.0);
+      e_y0 = ((gdouble) height * 0.75) - (line_length[4] / 2.0);
+    }
+    break;
+    case 2:
+    {
+      a_x0 = ((gdouble) width * 0.75) - line_length[0];
+      a_y0 = ((gdouble) height * 0.75) - line_length[0];
+
+      b_x0 = ((gdouble) width * 0.75) - line_length[1];
+      b_y0 = ((gdouble) height * 0.75) + line_length[1];
+
+      c_x0 = ((gdouble) width * 0.75) + line_length[2];
+      c_y0 = ((gdouble) height * 0.75) + line_length[2];
+
+      d_x0 = ((gdouble) width * 0.75) + line_length[3];
+      d_y0 = ((gdouble) height * 0.75) - line_length[3];
+
+      e_x0 = ((gdouble) width * 0.75) - (line_length[4] / 2.0);
+      e_y0 = ((gdouble) height * 0.75) - (line_length[4] / 2.0);
+    }
+    break;
+    case 3:
+    {
+      a_x0 = ((gdouble) width * 0.75) - line_length[0];
+      a_y0 = ((gdouble) height * 0.25) - line_length[0];
+
+      b_x0 = ((gdouble) width * 0.75) - line_length[1];
+      b_y0 = ((gdouble) height * 0.25) + line_length[1];
+
+      c_x0 = ((gdouble) width * 0.75) + line_length[2];
+      c_y0 = ((gdouble) height * 0.25) + line_length[2];
+
+      d_x0 = ((gdouble) width * 0.75) + line_length[3];
+      d_y0 = ((gdouble) height * 0.25) - line_length[3];
+
+      e_x0 = ((gdouble) width * 0.75) - (line_length[4] / 2.0);
+      e_y0 = ((gdouble) height * 0.25) - (line_length[4] / 2.0);
+    }
+    break;
+    }
+    
+    a_width = line_length[0];
+    a_height = line_length[0];
+    
+    b_width = line_length[1];
+    b_height = line_length[1];
+
+    c_width = line_length[2];
+    c_height = line_length[2];
+
+    d_width = line_length[3];
+    d_height = line_length[3];
+
+    e_width = line_length[4];
+    e_height = line_length[4];
+
+    /* square a - #0 */
+    cairo_rectangle(cr,
+		    a_x0, a_y0,
+		    a_width, a_height);
+    cairo_fill(cr);
+
+    /* square b - #0 */
+    cairo_rectangle(cr,
+		    b_x0, b_y0,
+		    b_width, b_height);
+    cairo_fill(cr);
+
+    /* square c - #0 */
+    cairo_rectangle(cr,
+		    c_x0, c_y0,
+		    c_width, c_height);
+    cairo_fill(cr);
+
+    /* square d - #0 */
+    cairo_rectangle(cr,
+		    d_x0, d_y0,
+		    d_width, d_height);
+    cairo_fill(cr);
+
+    /* square e - #0 */
+    cairo_rectangle(cr,
+		    e_x0, e_y0,
+		    e_width, e_height);
+    cairo_fill(cr);  
+  
+    cairo_rotate(cr,
+		 angle);
+  
+    cairo_pop_group_to_source(cr);
+  }
+
   //TODO:JK: implement me
 }
 
@@ -75,26 +253,42 @@ monolight_animation_util_render_cross(cairo_t *cr,
 				      guint r, guint g, guint b, guint a)
 {
   gdouble line_width[5];
-  
+
+  gdouble correction;
+  gdouble frequency;
+  gdouble magnitude;  
   gdouble a_pos_x0, a_pos_y0;
   gdouble a_pos_x1, a_pos_y1;
   gdouble b_pos_x0, b_pos_y0;
   gdouble b_pos_x1, b_pos_y1;
   gdouble max_line_width;
-  guint i;
+  guint nth;
+  guint i, k;
+
+  width *= scale;
+  height *= scale;
   
-  max_line_width = 3.0;
+  max_line_width = scale * 4.0;
 
   for(i = 0; i < 5; i++){
     line_width[i] = max_line_width;
   }
   
-  for(i = 0; i < buffer_end - buffer_start; i++){
-    gdouble value;
+  correction = (gdouble) samplerate / (gdouble) buffer_size;
+  magnitude = 0.0;
+  
+  for(nth = buffer_start + 1, i = 0, k = 0; i < buffer_end - buffer_start && nth < buffer_size / 2; i++, nth++){
+    frequency = nth * correction;
+    
+    magnitude += magnitude_buffer[nth];
+    k++;
 
-    value = magnitude_buffer[buffer_start + i];  
+    if(nth % ((buffer_end - buffer_start) / 5) == 0){
+      line_width[(guint) floor((double) i / (double) (buffer_end - buffer_start) * 5.0)] *= (magnitude / (buffer_end - buffer_start));
 
-    line_width[(guint) floor((double) i / (double) (buffer_end - buffer_start) * 5.0)] *= value;
+      magnitude = 0.0;
+      k = 0;
+    }
   }
 
   cairo_set_source_rgba(cr,
